4dac5663387256d2e5a1b8026b0eea3512a8778a
Gpap Peaky <giorgospapamat@gmail.com>
2024-02-18 18:45:22 +0200

    Initialise Window Fix

--- src/init_window.c
+++ src/init_window.c
@@ -1,8 +1,1 @@
 #include "init_window.h"
-
-#ifndef INIT_WINDOW
-#define INIT_WINDOW
-
-INITIALISER init_win();
-
-#endif
--- src/init_window.h
+++ src/init_window.h
@@ -1,1 +1,8 @@
 #include "include.aux"
+
+#ifndef INIT_WINDOW
+#define INIT_WINDOW
+
+INITIALISER init_win();
+
+#endif
