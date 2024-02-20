278c372a9c54953d7fa954773bd35509fb5e95ad
Gpap Peaky <giorgospapamat@gmail.com>
2024-02-20 23:35:14 +0200

    Revisioned main loop

--- src/init_window.c
+++ src/init_window.c
@@ -4,29 +4,31 @@ SDL_Window * win = NULL;
 SDL_Renderer * rnd = NULL;
 
 INITIALISER init_win(){
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1){
         printf("SDL Could Not Initialise, Error: %s\n",SDL_GetError());
-        exit(EXIT_FAILURE);
+        return;
     }
 
+    IMG_Init(IMG_INIT_PNG);
     if(IMG_Init(IMG_INIT_PNG) == 0){
         printf("Failed To Initialise PNGs, Error: %s\n",SDL_GetError());
-        exit(EXIT_FAILURE);
+        return;
     }
 
     /* Initialising window and renderer */
 
-    win = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    win = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 800, SDL_WINDOW_SHOWN);
     if(win == NULL){
         printf("Failed To Create Window: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
+        return;
     }
 
     rnd = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(rnd == NULL){
         printf("Failed To Create Renderer: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
+        return;
     }
 
-    return;
+    SDL_RenderClear(rnd);
 }
--- src/main.c
+++ src/main.c
@@ -4,23 +4,26 @@ int main(int argc, char* argv[]){
 
     init_win(); /* Initialise */
 
+    printf("Works!\n");
+
     int quit = 0;
-    SDL_Event e;
+    SDL_Event e;    
 
     while(!quit){
         while(SDL_PollEvent(&e) != 0){
-            if(e.type == SDL_QUIT){
+            if(e.key.keysym.sym == SDLK_ESCAPE){
                 quit = 1;
             }
         }
-
-        SDL_SetRenderDrawColor(rnd, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_SetRenderDrawColor(rnd, 0, 0, 255, 255);
         SDL_RenderClear(rnd);
-
-        /* Render Stuff Here */
-
         SDL_RenderPresent(rnd);
+        SDL_Delay(100);
     }
+
+    SDL_DestroyRenderer(rnd);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
