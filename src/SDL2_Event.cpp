aa32bb6ccccc093613bcf55bfd6e6fe07237b022
GpapPeaky <giorgospapamat@gmail.com>
2024-12-20 00:24:23 +0200

    Added player movement

--- src/SDL2_Event.cpp
+++ src/SDL2_Event.cpp
@@ -7,6 +7,37 @@ void SDL2_HandleEvents(bool& quit){
         if(e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && (e.key.keysym.sym == SDLK_ESCAPE || e.key.keysym.sym == SDLK_e))){
             quit = true;
         }
+
+        SDL2_HandlePlayerMovement(BSC_Entities[PLAYER][0], e);
+    }
+
+    return;
+}
+
+void SDL2_HandlePlayerMovement(BSC_Entity* entity, SDL_Event e){
+    if(e.type == SDL_KEYDOWN || e.type == SDL_KEYUP){
+        bool keyPressed = (e.type == SDL_KEYDOWN);
+
+        switch(e.key.keysym.sym){
+            case SDLK_w: // Move up
+                if(keyPressed) entity->img.position.y -= entity->stats.baseSpd;
+                break;
+
+            case SDLK_s: // Move down
+                if(keyPressed) entity->img.position.y += entity->stats.baseSpd;
+                break;
+
+            case SDLK_a: // Move left
+                if(keyPressed) entity->img.position.x -= entity->stats.baseSpd;
+                break;
+
+            case SDLK_d: // Move right
+                if(keyPressed) entity->img.position.x += entity->stats.baseSpd;
+                break;
+
+            default:
+                break;
+        }
     }
 
     return;
--- src/SDL2_Event.hpp
+++ src/SDL2_Event.hpp
@@ -1,4 +1,5 @@
 #include "../ThirdParty/SDL2/include/SDL2/SDL.h"
+#include "BSC_Entity.hpp"
 
 #ifndef __SDL2_EVENT_HPP__
 #define __SDL2_EVENT_HPP__
@@ -9,5 +10,13 @@
  * @param quit Checks if we have quit the main loop
  */
 void SDL2_HandleEvents(bool& quit);
+
+/**
+ * @brief Handle the player movement
+ * 
+ * @param entity Player Entity
+ * @param e Event
+ */
+void SDL2_HandlePlayerMovement(BSC_Entity* entity, SDL_Event e);
 
 #endif
